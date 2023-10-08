63d9b9943c55aa33c91559cdd00f7fcdb74703a6
IT20405090 <IT20405090@my.sliit.lk>
2023-10-08 11:47:11 +0530

    home page, page alignments, create pages

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,34 +1,44 @@
 import React from "react";
-
+import "./Home.css";
+import { Container, ButtonGroup, Button } from 'react-bootstrap';
 
 const Home = () => {
-
-	return (
-		<section className="homepage">
-
-
-					<div className = "home" >
-					<h1><b>Travel Reservation Web Application </b></h1>
-					</div>
-					
-					<br /><br/>
-					<br/> <br/>
-					<div className="buttons">
-              <a href="/train-management" className="btn btn-primary">Train Management</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  {/* <a href="/train-management" className="btn btn-primary">Login</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <a href="/train-management" className="btn btn-primary">Register</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
+    return (
+        <section className="homepage">
+            <div className="home">
+                <h1 className="main-title"><b>Travel Reservation Web Application</b></h1>
+            </div>
+            <br /><br />
+            <br /> <br />
+            <div className="buttons">
+                <div>
+                    <div className="header">
+                        <Container>
+                            <h5>Seamless Travel Experiences Await You, Empowering Your Journeys: Introducing the Next Generation Ticket Reservation System</h5>
+                        </Container>
+                    </div>
+                    <div className="content">
+                        <div className="blurred-background"></div>
+                        <Container>
+                            <div className="card">
+                                <ButtonGroup className="button-container">
+                                    <Button variant="dark" size="lg" className="quarter-button" style={{ borderRadius: '0' }} href="/train-management">Train Schedule</Button>
+                                    <Button variant="dark" size="lg" className="quarter-button" style={{ borderRadius: '0' }} href="/ticket-create">Ticket Booking</Button>
+                                    <Button variant="dark" size="lg" className="quarter-button" style={{ borderRadius: '0' }} href="">Reserve a Train</Button>
+                                    <Button variant="dark" size="lg" className="quarter-button" style={{ borderRadius: '0' }} href="">Activate Account</Button>
+                                </ButtonGroup>
+                            </div>
+                            {/* 
               <a href="/train-create" className="btn btn-success">Create Train Schedule</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <a href="/train-update" className=" btn btn-info">Update Train Schedule</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <a href="/ticket-management" className="btn btn-secondary">Ticket Booking Management</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <a href="/ticket-create" className="btn btn-primary">Ticket Booking</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="/train-update" className=" btn btn-info">Update Train Schedule</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
               <a href="/ticket-update" className="btn btn-success">Ticket Booking Update</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <a href="/#" className="btn btn-info">Travel Management</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </div>
-				
-				
-			
-		</section>
-	);
+              <a href="/#" className="btn btn-info">Travel Management</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
+                        </Container>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
