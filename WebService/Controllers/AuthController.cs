2440ef343ffc6a8726356f560ae0f92f921934f9
vihaan1997 <vihaan1997@gmail.com>
2023-10-17 19:18:03 +0530

    Changes related to the web service.

---
+++ WebService/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelWebService.Model;
+using TravelWebService.Services;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly UserService _userService;
+
+    public AuthController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(Login loginModel)
+    {
+        var user = await _userService.GetByUsernameAsync(loginModel.Email);
+
+        if (user == null || user.Password != loginModel.Password)
+        {
+            // Authentication failed
+            return BadRequest(new { message = "Login Failed", isAuthenticated = false });
+        }
+
+        // Authentication succeeded
+        return Ok(new { message = "Login Succeeded", isAuthenticated = true, user });
+    }
+}
