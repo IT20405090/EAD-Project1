3cbbca0bb0af70d69d99f268fee4d05cc3854c12
Disni Jayawickrama <it20147778@my.sliit.lk>
2023-10-18 13:17:44 +0530

    create schedule train changes

--- frontend/src/components/Train/CreateTrainSchehdule.js
+++ frontend/src/components/Train/CreateTrainSchehdule.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import $ from 'jquery'; // Import jQuery
+import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js';
-import "./TrainSchedule.css"
+import './TrainSchedule.css';
 import { Link, useNavigate } from 'react-router-dom';
 
 const CreateTrainSchedule = () => {
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    date:'',
-    startTime:'',
-    endTime:'',
-    startingStation:'',
-    endingStation:'',
-    duration:'',
-    trainName:'',
-    engineNumber:'',
-    availableSeats:'',
-    specialNotices:'',
+    date: '',
+    startTime: '',
+    endTime: '',
+    startingStation: '',
+    endingStation: '',
+    duration: '',
+    trainName: '',
+    trainEngine: '',
+    availableSeats: '',
+    specialNotices: '',
   });
 
   const handleSubmit = (e) => {
@@ -30,15 +30,14 @@ const CreateTrainSchedule = () => {
     const scheduleData = {
       Date: formData.date,
       StartTime: formData.startTime,
-      TrainEngine: formData.trainEngine,
-      StartingStation: formData.startingStation,
-      AvailableSeats: formData.availableSeats,
-      Duration: formData.duration,
       EndTime: formData.endTime,
-      TrainName: formData.trainName,
+      StartingStation: formData.startingStation,
       EndingStation: formData.endingStation,
-      SpecialNotices: formData.specialNotices
-      // Map other form fields here
+      Duration: formData.duration,
+      TrainName: formData.trainName,
+      TrainEngine: formData.trainEngine,
+      AvailableSeats: formData.availableSeats,
+      SpecialNotices: formData.specialNotices,
     };
 
     // Send a POST request to your API
@@ -79,136 +78,212 @@ const CreateTrainSchedule = () => {
 
   return (
     <div className="container-fluid">
-    <div className="bg-light">
-    <div className="container mt-5">
-      <h2 className="TrainScehdule_topic" >Create Train Schedule </h2> <br/>
-      <form onSubmit={handleSubmit}>
-        <div className="row" style={{ textAlign: 'left', color:'gray', fontSize:'18px' }}>
-          <div className="col-md-6">
-          
-          <hr/>
+      <div className="bg-light">
+        <div className="container mt-5">
+          <h2 className="TrainScehdule_topic">Create Train Schedule</h2> <br />
+          <form onSubmit={handleSubmit}>
+            <div className="row" style={{ textAlign: 'left', color: 'gray', fontSize: '18px' }}>
+              <div className="col-md-6">
+                <hr />
 
-            <div className="mb-3">
-              <label htmlFor="date" className="form-label">Date</label>
-              <input type="text" className="form-control datepicker" id="date" placeholder='Enter the date'  required />
-            </div>
-            
-            <div className="mb-3">
-              <label htmlFor="startTime" className="form-label">Start Time</label>
-              <input type="time" className="form-control" id="startTime" placeholder='Enter the startug time' required />
-            </div>
+                <div className="mb-3">
+                  <label htmlFor="date" className="form-label">
+                    Date
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control datepicker"
+                    id="date"
+                    name="date"
+                    placeholder="Enter the date"
+                    required
+                    value={formData.date}
+                    onChange={handleInputChange}
+                  />
+                </div>
 
-            <br/>
-            <hr/>
-            <div className="mb-3">
-<<<<<<< HEAD
-            <label htmlFor="trainEngine" className="form-label">Train Engine</label>
-            <input type="text" className="form-control" id="engine" placeholder='Enter the engine number' required /> 
-            </div>
-=======
-            <label htmlFor="engine" className="form-label">
-              Train Engine
-            </label>
-            <select className="form-select" id="engine" required>
-              <option value="">Select Train Engine</option>
-              <option value={1001}>1001</option>
-              <option value={1002}>1002</option>
-              <option value={1003}>1003</option>
-              <option value={1004}>1004</option>
-              <option value={1005}>1005</option>
-              <option value={1006}>1006</option>
-              <option value={1007}>1007</option>
-              <option value={1008}>1008</option>
-              <option value={1009}>1009</option>
-              <option value={1010}>1010</option>
-            </select>
-          </div>
+                <div className="mb-3">
+                  <label htmlFor="startTime" className="form-label">
+                    Start Time
+                  </label>
+                  <input
+                    type="time"
+                    className="form-control"
+                    id="startTime"
+                    name="startTime"
+                    placeholder="Enter the starting time"
+                    required
+                    value={formData.startTime}
+                    onChange={handleInputChange}
+                  />
+                </div>
 
->>>>>>> f0d8bd593037f5adecdc6688a2c6c004f08ca149
+                <br />
+                <hr />
+                <div className="mb-3">
+                  <label htmlFor="trainEngine" className="form-label">
+                    Train Engine
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="trainEngine"
+                    name="trainEngine"
+                    placeholder="Enter the engine number"
+                    required
+                    value={formData.trainEngine}
+                    onChange={handleInputChange}
+                  />
+                </div>
 
-            <div className="mb-3">
-              <label htmlFor="startingStation" className="form-label">Starting Station</label>
-              <select className="form-select" id="startingStation" placeholder='Select the starting station' required>
-                <option value="" disabled selected>Select Starting Station</option>
-                <option>Gampaha</option>
-                <option>Colombo-Fort</option>
-                <option>Colombo-Maradana</option>
-                <option>Colombo-Dematagoda</option>
-                <option>Moratuwa</option>
-                <option>Panadura</option>
-
-              </select>
-            </div>
-
-            <div className="mb-3">
-              <label htmlFor="availableSeats" className="form-label">Available Seats</label>
-              <input type="text" className="form-control" id="trainId" placeholder='Enter the available seats count' required />
-            </div>
-          </div>
-
-      
-
-          <div className="col-md-6">
-          
-            <hr/>
-
-            <div className="mb-3">
-              <label htmlFor="duration" className="form-label">Estimated Duration (hours)</label>
-              <input type="number" className="form-control" id="duration" placeholder='Enter the duration' required />
-            </div>
-
-            <div className="mb-3">
-            <label htmlFor="endTime" className="form-label">End Time</label>
-            <input type="time" className="form-control" id="endTime" placeholder='Enter the ending time' required />
-            </div>
-
-            <br/>
-            <hr/>
-            <div className="mb-3">
-              <label htmlFor="trainName" className="form-label">Train Name</label>
-              <input type="text" className="form-control" id="trainName" placeholder='Enter the train name' required />
-            </div>
-
-            <div className="mb-3">
-            <label htmlFor="endingStation" className="form-label">Ending Station</label>
-              <select className="form-select" id="endingStation" placeholder='Select the ending station' required>
-                <option value="" disabled selected>Select Ending Station</option>
-                <option>Gampaha</option>
-                <option>Colombo-Fort</option>
-                <option>Colombo-Maradana</option>
-                <option>Colombo-Dematagoda</option>
-                <option>Moratuwa</option>
-                <option>Panadura</option>
-              </select>
-            </div>
-          </div>
-
-            
-            <br/>
-            <hr/>
-            <div className="mb-3">
-                <label htmlFor="specialNotices" className="form-label">Special Notices</label>
-                <textarea className="form-control" id="specialNotices" rows="4" placeholder='Type here ...'></textarea>
+                <div className="mb-3">
+                  <label htmlFor="engine" className="form-label">
+                    Train Engine
+                  </label>
+                  <select
+                    className="form-select"
+                    id="engine"
+                    name="trainEngine"
+                    required
+                    value={formData.trainEngine}
+                    onChange={handleInputChange}
+                  >
+                    <option value="">Select Train Engine</option>
+                    <option value="1001">1001</option>
+                    <option value="1002">1002</option>
+                    <option value="1003">1003</option>
+                    <option value="1004">1004</option>
+                    <option value="1005">1005</option>
+                    <option value="1006">1006</option>
+                    <option value="1007">1007</option>
+                    <option value="1008">1008</option>
+                    <option value="1009">1009</option>
+                    <option value="1010">1010</option>
+                  </select>
+                </div>
               </div>
 
-        </div>
+              <div className="col-md-6">
+                <hr />
 
-        <div className="mb-3">
-        <Link to="/train-management">
-        <button type="submit" className="btn btn-secondary" style={{ marginRight: '20%', width: '110px' }}>
-            Back
-          </button>
-        </Link>
-          <button type="submit" className="btn btn-primary" >
-            Submit
-          </button>
-          
-        </div>
-        <br/>
+                <div className="mb-3">
+                  <label htmlFor="duration" className="form-label">
+                    Estimated Duration (hours)
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="duration"
+                    name="duration"
+                    placeholder="Enter the duration"
+                    required
+                    value={formData.duration}
+                    onChange={handleInputChange}
+                  />
+                </div>
 
-      </form>
-    </div>
-    </div>
+                <div className="mb-3">
+                  <label htmlFor="endTime" className="form-label">
+                    End Time
+                  </label>
+                  <input
+                    type="time"
+                    className="form-control"
+                    id="endTime"
+                    name="endTime"
+                    placeholder="Enter the ending time"
+                    required
+                    value={formData.endTime}
+                    onChange={handleInputChange}
+                  />
+                </div>
+
+                <br />
+                <hr />
+                <div className="mb-3">
+                  <label htmlFor="trainName" className="form-label">
+                    Train Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="trainName"
+                    name="trainName"
+                    placeholder="Enter the train name"
+                    required
+                    value={formData.trainName}
+                    onChange={handleInputChange}
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="endingStation" className="form-label">
+                    Ending Station
+                  </label>
+                  <select
+                    className="form-select"
+                    id="endingStation"
+                    name="endingStation"
+                    required
+                    value={formData.endingStation}
+                    onChange={handleInputChange}
+                  >
+                    <option value="">Select Ending Station</option>
+                    <option>Gampaha</option>
+                    <option>Colombo-Fort</option>
+                    <option>Colombo-Maradana</option>
+                    <option>Colombo-Dematagoda</option>
+                    <option>Moratuwa</option>
+                    <option>Panadura</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="availableSeats" className="form-label">
+                  Available Seats
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="availableSeats"
+                  name="availableSeats"
+                  placeholder="Enter the available seats count"
+                  required
+                  value={formData.availableSeats}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="specialNotices" className="form-label">
+                Special Notices
+              </label>
+              <textarea
+                className="form-control"
+                id="specialNotices"
+                name="specialNotices"
+                rows="4"
+                placeholder="Type here ..."
+                value={formData.specialNotices}
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+
+            <div className="mb-3">
+              <Link to="/train-management">
+                <button type="button" className="btn btn-secondary" style={{ marginRight: '20%', width: '110px' }}>
+                  Back
+                </button>
+              </Link>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+          <br/><br/><br/><br/><br/>
+        </div>
+      </div>
     </div>
   );
 };
